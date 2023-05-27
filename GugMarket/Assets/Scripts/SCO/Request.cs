334ad044077c33dce9c7806ed5f54be69e914528
Sheep-Sinnesael <124142591+Sheep-Sinnesael@users.noreply.github.com>
2023-05-27 18:14:09 +0200

    Assignation Requet

--- GugMarket/Assets/Scripts/SCO/Request.cs
+++ GugMarket/Assets/Scripts/SCO/Request.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SCO_Request", menuName = "GugObject/Request")]
 public class Request : ScriptableObject
 {
-    [ResizableTextArea] public string linkedRequest;
+    public string linkedRequest;
     [Expandable] public Recipe aimedRecipe;
 }
