4b093aa552e31461b5a7d56efcaaf3fd61543bed
Arthur Deleye <arthurdeleye@gmail.com>
2023-05-27 19:34:16 +0200

    Merge Fix

--- GugMarket/Assets/Scripts/SCO/RequestData.cs
+++ GugMarket/Assets/Scripts/SCO/RequestData.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SCO_Request", menuName = "GugObject/Request")]
 public class RequestData : ScriptableObject
 {
-<<<<<<< HEAD:GugMarket/Assets/Scripts/SCO/RequestData.cs
-    [ResizableTextArea] public string linkedRequest;
+    [TextArea(3,8)] public string linkedRequest;
     [Expandable] public RecipeData aimedRecipe;
-=======
-    public string linkedRequest;
-    [Expandable] public Recipe aimedRecipe;
->>>>>>> Mathys:GugMarket/Assets/Scripts/SCO/Request.cs
 }
